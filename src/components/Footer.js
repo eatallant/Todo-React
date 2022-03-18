7214d49ddf1f8dd7ac4647dae5fcf524f3a9d6df
Eric Tallant <eatallant@gmail.com>
2022-03-18 15:32:05 -0500

    added active toggle highlighting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,38 @@
 import React from "react";
 
 const Footer = ({ todos, count, setShown }) => {
-  const setShownToggle = (status) => {
+  const setShownToggle = (status, e) => {
     setShown(status);
+    document.querySelector(".status-selector-item-active").className = "";
+    e.target.className = "status-selector-item-active";
   };
   return todos.length ? (
     <div className='footer-container'>
       <div className='todo-count footer-flex'>Remaining items: {count}</div>
       <ul className='status-selector-list footer-flex'>
         <li className='status-selector-item'>
-          <a href='/#' onClick={() => setShownToggle("SHOW_ALL")}>
+          <a
+            className='status-selector-item-active'
+            href='/#'
+            onClick={(e) => setShownToggle("SHOW_ALL", e)}
+          >
             Show All
           </a>
         </li>
         <li className='status-selector-item'>
-          <a href='/#/active' onClick={() => setShownToggle("SHOW_ACTIVE")}>
+          <a
+            className='test'
+            href='/#/active'
+            onClick={(e) => setShownToggle("SHOW_ACTIVE", e)}
+          >
             Active
           </a>
         </li>
         <li className='status-selector-item'>
           <a
+            className=''
             href='/#/completed'
-            onClick={() => setShownToggle("SHOW_COMPLETED")}
+            onClick={(e) => setShownToggle("SHOW_COMPLETED", e)}
           >
             Completed
           </a>
