fe31a7b730b1a117130377b41f3cb48be13c9205
Eric Tallant <eatallant@gmail.com>
2022-03-18 11:13:04 -0500

    Delete unused file

--- src/components/Model.js
+++
@@ -1,6 +0,0 @@
-export default function Model(todos) {
-  console.log("Input provided to Model:", todos);
-  return todos
-    ? localStorage.setItem("todos", JSON.stringify(todos))
-    : JSON.parse(localStorage.getItem("todos")) || [];
-}
